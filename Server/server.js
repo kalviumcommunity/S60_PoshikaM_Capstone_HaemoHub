33173653d92efe7f9220978c14167f76a124e420
Poshika <poshika@Poshikas-MacBook-Air.local>
2024-04-27 19:38:11 +0530

    connected server with mongodb and created database

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,17 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
 const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.CONNECTDB_URL)
+.then(() => {
+    console.log("Connect with mongodb database")
+})
+.catch(() =>{
+    console.log("error in connecting")
+})
 
 app.get("/", (request, response) => {
     response.send("Hi ! Iam server")

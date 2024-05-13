0be7db0deb7e1bc0b217f7b4ffdd853cd420076d
Poshika <poshika@Poshikas-MacBook-Air.local>
2024-05-13 15:18:48 +0530

    added some images and styled the about page using tailwind css

--- Client/src/Components/About.jsx
+++ Client/src/Components/About.jsx
@@ -4,50 +4,63 @@ function About(){
         <div className="gap-y-2">
             <div>
                 <h1 className="text-4xl">About HaemoHub</h1>
-                <p>Welcome to HaemoHub, where technology meets altruism to save lives and build a community of hope. Our platform is a beacon of light for those in need of blood transfusions. The go-to platform for finding blood donations nearby and saving lives in your community. Powered by technology and fueled by compassion, HaemoHub simplifies the process of locating blood donors and nearby blood banks, ensuring timely access to life-saving resources..</p>
+                <p className="mt-6">Welcome to HaemoHub, where technology meets altruism to save lives and build a community of hope. Our platform is a beacon of light for those in need of blood transfusions. The go-to platform for finding blood donations nearby and saving lives in your community. Powered by technology and fueled by compassion, HaemoHub simplifies the process of locating blood donors and nearby blood banks, ensuring timely access to life-saving resources..</p>
             </div>
-            <div className="mt-3.5">
+
+
+            <div className="mt-20 flex place-content-center gap-x-36">
                 <div>
-                    <h1 className="text-xl">Our Mission</h1>
+                    <img className="h-64 w-94" src="https://bisanibrothers.com/images/our-mission.png" alt="" />
+                    {/* <h1 className="text-xl">Our Mission</h1> */}
                 </div>
                 <div>
-                    <p> At HaemoHub, our mission is to harness the power of technology to make blood donation accessible and efficient, ultimately saving lives in the process. We are dedicated to connecting donors with recipients in need, prioritizing speed and convenience without compromising on the quality of care. We believe in the power of collective action and the profound impact it can have on healthcare outcomes.</p>
-                </div>
-            </div>
-            <div className="mt-3.5">
-                <div>
-                    <h1 className="text-xl">Lifesaving Impact</h1>
-                </div>
-                <div>
-                    <p>HaemoHub is at the forefront of life-saving initiatives, streamlining the process of finding blood donations in your area. By providing real-time access to nearby blood banks and donors, we ensure that patients receive the critical care they need when they need it most.</p>
+                    <p className="w-96"> At HaemoHub, our mission is to harness the power of technology to make blood donation accessible and efficient, ultimately saving lives in the process. We are dedicated to connecting donors with recipients in need, prioritizing speed and convenience without compromising on the quality of care. We believe in the power of collective action and the profound impact it can have on healthcare outcomes.</p>
                 </div>
             </div>
-            <div className="mt-3.5">
+
+
+            <div className="mt-20 flex place-content-center gap-x-36">
                 <div>
-                    <h1 className="text-xl">Community Empowerment</h1>
+                    <p className="w-96">HaemoHub is at the forefront of life-saving initiatives, streamlining the process of finding blood donations in your area. By providing real-time access to nearby blood banks and donors, we ensure that patients receive the critical care they need when they need it most.</p>
                 </div>
                 <div>
-                    <p>Beyond saving lives, HaemoHub empowers communities to come together and make a difference. Through our platform, individuals can contribute to the well-being of others, fostering a culture of empathy, support, and altruism. Whether you are in need of blood or looking to donate, HaemoHub is here to support you every step of the way.</p>
+                    {/* <h1 className="text-xl">Lifesaving Impact</h1> */}
+                    <img className="h-64 w-94" src="https://previews.123rf.com/images/olivkairishka/olivkairishka1805/olivkairishka180500013/100738083-the-logo-of-the-word-life-heart-and-cardiogram-logo-life-on-a-white-background.jpg" alt="" />
                 </div>
             </div>
-            <div className="mt-3.5">
+
+
+            <div className="mt-20 flex place-content-center gap-x-36">
                 <div>
-                    <h1 className="text-xl">Our Approach</h1>
+                    {/* <h1 className="text-xl">Community Empowerment</h1> */}
+                    <img className="h-64 w-94" src="https://pbs.twimg.com/profile_images/1016324707078066177/e_34EFhl_400x400.jpg" alt="" />
                 </div>
                 <div>
+                    <p className="w-96">Beyond saving lives, HaemoHub empowers communities to come together and make a difference. Through our platform, individuals can contribute to the well-being of others, fostering a culture of empathy, support, and altruism. Whether you are in need of blood or looking to donate, HaemoHub is here to support you every step of the way.</p>
+                </div>
+            </div>
+
+
+            <div className="mt-20 flex place-content-center gap-x-36">
+                <div className="w-96">
                     <p>HaemoHub offers a user-friendly search experience, designed to connect blood donors and recipients seamlessly:</p>
+                        <p><b>Search Nearby: </b>Use our search feature to locate blood banks and donors in your area. With just a few clicks, you can find the resources you need to save lives.</p>
+                        <p><b>Real-time Availability: </b>Access real-time information about blood availability and donor locations, ensuring timely access to life-saving resources.</p>
                 </div>
                 <div>
-                        <p><b>Search Nearby:</b>Use our search feature to locate blood banks and donors in your area. With just a few clicks, you can find the resources you need to save lives.</p>
-                        <p><b>Real-time Availability:</b>Access real-time information about blood availability and donor locations, ensuring timely access to life-saving resources.</p>
+                    {/* <h1 className="text-xl">Our Approach</h1> */}
+                    {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgFwsrjXQf3p5WqrXKmc-oa1ujcL9TrMKlsg&usqp=CAU" alt="" /> */}
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOc6ISgORsoQoUqTxrs5y2g669aopmkGcVOw&usqp=CAU" alt="" />
                 </div>
             </div>
-            <div className="mt-3.5">
+
+
+            <div className="mt-20 place-content-center gap-x-20">            
                 <div>
-                <h1 className="text-4xl">Join the Movement</h1>
+                    <h1 className="text-4xl">Join the Movement</h1>
                 </div>
                 <div>
-                    <p>HaemoHub invites you to join our community of life-savers and make a difference today. Together, we can ensure that no one faces a shortage of blood when they need it most.</p>
+                    <p className="mt-6">HaemoHub invites you to join our community of life-savers and make a difference today. Together, we can ensure that no one faces a shortage of blood when they need it most.</p>
                 </div>
                 <div>
                     <p>Thank you for choosing HaemoHub. Lets save lives, one search at a time.</p>
